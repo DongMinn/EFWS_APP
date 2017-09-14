f11cf84973919d76850ef17daab0a4052f3f1289
DDongMin <dong4753@naver.com>
2017-09-14 16:25:38 +0900

    0914

--- src/components/customerReservation/CustomerReservationStateView.js
+++ src/components/customerReservation/CustomerReservationStateView.js
@@ -49,7 +49,7 @@ class CustomerReservationStateView extends Component {
         }
     }
     handleTitle() {
-        if (this.props.customerData.remainingWaitingTime === "0") return '지금 입장하세요!'
+        if (this.props.customerData.waitingState === "WAIT") return '지금 입장하세요!'
         else if (this.props.availableCheck === false) return '대기정보가 없습니다!'
         return this.props.customerData.remainingWaitingTime + '분 후 입장가능!'
     }
