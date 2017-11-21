d0dc497f01f7a0c0192c11eb05dea33f820544fc
DDongMin <dong4753@naver.com>
2017-11-21 16:26:42 +0900

    Change reservation state color ver2.1++"

--- src/components/reservation/ReservationStateView.js
+++ src/components/reservation/ReservationStateView.js
@@ -235,7 +235,7 @@ class ReservationStateTestView extends Component {
                             <NotificationsIcon />
                         </Badge>
                     </CardHeader>
-
+ 
                     <CardActions>
 
                         <Card>
